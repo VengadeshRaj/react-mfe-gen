0861c237f2cf6c4dad39a2ad108c017e1199b769
vengadeshraj <vengadeshraj405@gmail.com>
2025-09-02 23:49:46 +0530

    added architecture disclaimer

--- mfe-gen.js
+++ mfe-gen.js
@@ -1,11 +1,12 @@
 import inquirer from "inquirer";
 import { PROMPTS } from "./constants.js";
 
-const mfeGen = async()=>{
-    
-   const result =  await inquirer.prompt(PROMPTS);
+const mfeGen = async () => {
+  console.log(
+    "👋 Hi there!\n\n⚙️ This tool currently supports micro-frontend creation with runtime integration through custom script injection.\n📌 Please consider this limitation when developing your MFE applications using this tool.\n"
+  );
+  const result = await inquirer.prompt(PROMPTS);
 
-    console.log('result',result)
-    
-}
+  console.log("result", result);
+};
 export default mfeGen;
